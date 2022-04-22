01bbe8496b819a5a77cbc6a757364af829a59b93
mudityadev <rmuditya@gmail.com>
2022-04-22 12:47:56 +0530

    started queue

--- Queues/basic.cpp
+++ Queues/basic.cpp
@@ -1,11 +1,16 @@
-//* Queue data structure 
+// ! Queue data structure 
 // * FIFO -> first-in first-out
+// * Use -> shared resource
 // * insertion -> rear or tail
 // * removal -> front or head
+
+// * Queue -> [1] -> [2] -> [3] -> [5] -> [...] -> [n]
 
 // ! operations
 // * Insertion -> Enqueue(x) or push(x)
 // * Removal   -> Dequeue()  or pop()
+// * lookup    -> front() or peek() 
+// * isEmpty   -> check empty
 
 #include<bits/stdc++.h>
 using namespace std;
