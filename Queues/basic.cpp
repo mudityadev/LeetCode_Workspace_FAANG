80d1d37f1314ee124d9f2a1190f49e39cfa82e6c
mudityadev <rmuditya@gmail.com>
2022-04-22 12:40:39 +0530

    Infix, prefix and postfix

---
+++ Queues/basic.cpp
@@ -0,0 +1,16 @@
+//* Queue data structure 
+// * FIFO -> first-in first-out
+// * insertion -> rear or tail
+// * removal -> front or head
+
+// ! operations
+// * Insertion -> Enqueue(x) or push(x)
+// * Removal   -> Dequeue()  or pop()
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+}
---
+++ Stacks/Infix_Prefix_Postfix.cpp
@@ -0,0 +1,21 @@
+// * Infix - Prefix - Postfix
+// ! Infix -> a + b, 1-2
+
+// ! Order of operation
+// * 1. Parentheses () {} []
+// * 2. Exponents 2^3 -> right to left <- 2^3^2 = 2^9
+// * 3. Multiplication ande divison left -> right
+// * 4. Addition and Subtraction left -> right
+
+
+// ! Prefix (Polish notation)
+// * <operator> <operand> <operand>
+// * +23 , -pq
+
+
+// ! Postfix Notation (reverse polish)
+// *  <operand> <operand> <operator>
+// * 2+3 -> 23+ , p-q -> pq-
+// *
+// *
+// *
