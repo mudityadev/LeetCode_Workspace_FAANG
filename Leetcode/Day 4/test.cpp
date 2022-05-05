9ba96113037a515bf85795a33fbe9b3a1a1e2b1e
mudityadev <rmuditya@gmail.com>
2022-05-05 08:07:39 +0530

    steak

--- Leetcode/Day 4/test.cpp
+++ Leetcode/Day 4/test.cpp
@@ -1,26 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-int sumOddLengthSubarrays(vector<int>& arr) {
-        int sum{0};
-        int n = arr.size(); // 5
-        int m = 1;
-        for (int i = 0; i < n ; i++)
-        {
-            sum+=arr[i];
-        }
-        if(n%2 == 0) return sum;
-        
-        for (int i = 0; i < n; i+=2)
-        {
-            /* code */
-        }
-        
 
-        
-    }
 int main()
 {
-    vector<int> v{1,4,2,5,3};
-
-    cout << sumOddLengthSubarrays(v) << " ";
+   string nn = "nrfzh";
+   string ans = "";
+   for (int i = 0; i < nn.length(); i++)
+   {
+    //    if(nn[i] == 
+       ans+= nn[i]+3;
+   }
+    cout << endl;
+   cout << ans << " ";
+   cout << 'z' - 'c' << " ";
 }
+   
