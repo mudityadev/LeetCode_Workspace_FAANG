14987061586e3c300bc89a13ad6c5e0a8369d1b6
mudityadev <rmuditya@gmail.com>
2022-05-07 09:49:27 +0530

    bit manipulation

--- Leetcode/231.power-of-two.cpp
+++ Leetcode/231.power-of-two.cpp
@@ -8,11 +8,16 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0) return false;
-        while(n%2==0){
-            n/=2;
-        }
-        return (n==1);
+        // * bit manipulation
+        if(n<=0) return false;
+        return ((n&(n-1)) == 0);
+
+        // * using modulus
+        // if(n==0) return false;
+        // while(n%2==0){
+        //     n/=2;
+        // }
+        // return (n==1);
     }
 };
 // @lc code=end
