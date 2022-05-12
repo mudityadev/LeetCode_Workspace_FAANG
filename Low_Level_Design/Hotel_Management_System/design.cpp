d65abbf3638dfc827dd6b383fba2fff5b1680faa
mudityadev <rmuditya@gmail.com>
2022-05-12 08:58:20 +0530

    hotel management system design completed, need to learn decorators inc pp

--- Low_Level_Design/Hotel_Management_System/design.cpp
+++ Low_Level_Design/Hotel_Management_System/design.cpp
@@ -1,6 +1,83 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+class Person{
+public:
+    string name;
+    Account accountDetails;
+    string phone;
+};
+
+enum AccountStatus{
+    ACTIVE, CLOSED, BLOCKED
+}
+
+class HouseKeeper : public Person{
+    vector<Room> getRoomServiced(Date date);
+};
+
+
+class Search{
+    vector<Room> searchRoom(RoomStyle roomStyle, Date startDate, int duration);
+};
+
+class Booking{
+    RoomBooking createBooking(Guest guestInfo);
+    RoomBooking cancelBooking(int bookingId);
+};
+
+class RoomBooking{
+    string bookingId;
+    Date startDate;
+    int durationInDays;
+    BookingStatus bookingStatus;
+    vector<Guest> guestList;
+    vector<Room> roomInfo;
+    BaseRoomCharge totalRoomCharges;
+};
+
+// Implement Decorators for extraCost and keeping adding if require
+// solve the room charge problem -> decorators used
+
+
+class Guest : public Person{
+    Search searchObj;
+    Booking bookingObj;
+
+};
+
+
+class Receptionist : public Person{
+    Search searchObj;
+    Booking bookingObj;
+
+    checkInGuest(Guest guest, RoomBooking bookingInfo);
+    checkOutGuest(Guest guest, RoomBooking bookingInfo);
+};
+
+class Admin : public Person{
+    void addRoom(Room roomDetail);
+    Room deleteRoom(string roomId);
+    void editRoom(Room roomDetail);
+};
+
+
+
+
+
+class Account{
+    string username;
+    string password;
+    // enum
+    AccountStatus accountStatus;
+};
+
+
+
+
+
+
 class Hotel{
 public:
     string name;
@@ -31,7 +108,19 @@ class RoomKey{
     bool isMaster;
 
     void assignRoom(Room room);
-}
+};
+
+class HouseKeepingLogs{
+    string description;
+    Date startDate;
+    int duration;
+    HouseKeeper housekeeper;
+
+    void addRoom(Room room);
+};
+
+
+
 
 
 class Location{
