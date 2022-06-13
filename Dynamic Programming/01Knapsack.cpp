5c742227f6f0bb094d27c1d09f54d0837c7c028d
mudityadev <rmuditya@gmail.com>
2022-06-13 20:43:08 +0530

    0 1 knapsack code

--- Dynamic Programming/01Knapsack.cpp
+++ Dynamic Programming/01Knapsack.cpp
@@ -3,7 +3,28 @@ using namespace std;
 #define ll long long
 #define vi vector<ll>
 
+int knapsack(vector<int> wt, vector<int> val, int capacity, int n){
+    if(n == 0 || capacity == 0) return 0;
+
+    if(wt[n-1]<capacity){
+        return max((wt[n-1]+knapsack(wt, val, capacity-wt[n-1], n-1)), knapsack(wt, val, capacity, n-1));
+    }
+
+    else if(wt[n-1]>capacity){
+        return knapsack(wt, val, capacity, n-1);
+    }
+}
+
+
+
 int main()
 {
-    
+    vector<int> wt {1,3,4,5};    
+    vector<int>val {1,4,5,7};    
+    int capacity = 7;
+    int n = wt.size();
+
+    cout << knapsack(wt, val, capacity, n) << " ";
+
+
 }
